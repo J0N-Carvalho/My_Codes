32fffdf5997b97ee83c83673a45370dad0d8353a
J0N-Carvalho <145946838+J0N-Carvalho@users.noreply.github.com>
2023-09-24 22:50:42 -0300

    Add files via upload
    Código básico, subtração entre dois números, feito em Java.

---
+++ Calculo.java
@@ -0,0 +1,12 @@
+public class Calculo {
+    public static void main(String[] args){
+        double n1, n2, sub;
+        
+        n1 = 50;
+        n2 = 35;
+        
+        sub = n1 - n2;
+        
+        System.out.println("A subtração dos números é: " + sub);
+    }
+}
